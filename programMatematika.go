3a462d03f8b25d1c4b7a4a7e84117c702d4b3b6f
ndo <ndo@192.168.1.5>
2020-06-18 21:29:54 +0700

    bangun ruang done

--- programMatematika.go
+++ programMatematika.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	var pilihBangun, pilihBangunDatar, pilihBangunRuang int
-	var p, l, t, s, a, r, luas float64
+	var p, l, t, s, a, r, luas, volume float64
 	phi := 3.14
 	fmt.Println("\n==============================================")
 	fmt.Println("Selamat Datang di Program Matematika Sederhana")
@@ -58,12 +58,12 @@ func main() {
 			fmt.Println(luas)
 		case 4:
 			fmt.Println("Luas lingkaran")
-			fmt.Print("Masukkan jari-jari")
+			fmt.Print("Masukkan jari-jari: ")
 			fmt.Scan(&r)
-			luas = phi * r * r
+			luas = phi * (r * r)
 			fmt.Println(luas)
 		default:
-			fmt.Println("salah")
+			fmt.Println("Bangun ruang tidak ada")
 		}
 	case 2:
 		fmt.Println("Volume Bangun Ruang")
@@ -71,8 +71,43 @@ func main() {
 		fmt.Println("2. Balok")
 		fmt.Println("3. Kerucut")
 		fmt.Println("4. Bola")
-		fmt.Println("5. Limas Segitiga")
+		fmt.Print("Pilih Bangun Ruang: ")
 		fmt.Scan(&pilihBangunRuang)
+		switch pilihBangunRuang {
+		case 1:
+			fmt.Println("Volume Kubus")
+			fmt.Print("Masukkan sisi: ")
+			fmt.Scan(&s)
+			volume = s * s * s
+			fmt.Println(volume)
+		case 2:
+			fmt.Println("Volume Balok")
+			fmt.Print("Masukkan panjang: ")
+			fmt.Scan(&p)
+			fmt.Print("Masukkan lebar: ")
+			fmt.Scan(&l)
+			fmt.Print("Masukkan tinggi: ")
+			fmt.Scan(&t)
+			volume = p * l * t
+			fmt.Println(volume)
+		case 3:
+			fmt.Println("Volume Kerucut")
+			fmt.Print("Masukkan jari-jari: ")
+			fmt.Scan(&r)
+			fmt.Print("Masukkan tinggi: ")
+			fmt.Scan(&t)
+			// di golang jika ingin membagi float harus menggunakan angka dibelakang koma. Jika tidak maka hasilnya akan dianggap integer
+			volume = (1.0 / 3.0) * phi * (r * r) * t
+			fmt.Println(volume)
+		case 4:
+			fmt.Println("Volume Bola")
+			fmt.Print("Masukkan jari-jari: ")
+			fmt.Scan(&r)
+			volume = (4.0 / 3.0) * phi * (r * r * r)
+			fmt.Println(volume)
+		default:
+			fmt.Println("Bangun ruang tidak ada")
+		}
 	default:
 		fmt.Println("Terima Kasih...")
 	}
